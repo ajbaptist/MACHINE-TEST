4fd04881b2689365f997452290db86ca701634d5
john baptist <ajbaptist18@gmail.com>
2021-08-03 22:42:50 +0100

    commit

--- task/lib/map/map.dart
+++ task/lib/map/map.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:task/map/mapdata.dart';
 
@@ -17,112 +16,56 @@ class _SimpleMapState extends State<SimpleMap> {
     controller = googleMapController;
   }
 
-  Future future() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.bestForNavigation);
-
-    setState(() {
-      mylocation = LatLng(position.latitude, position.longitude);
-      isChecked = true;
-      controller!.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-          zoom: 10, target: LatLng(position.latitude, position.longitude))));
-
-      print({mylocation, isChecked});
-    });
-  }
-
-  @override
-  void initState() {
-    future();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: future,
-        child: Icon(Icons.location_on),
-      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text('Google Map'),
-        actions: [IconButton(onPressed: null, icon: Icon(Icons.refresh_sharp))],
       ),
       body: GoogleMap(
           myLocationEnabled: true,
           markers: {
             Marker(
-                infoWindow: InfoWindow(
-                    title: isChecked == false
-                        ? 'ENSURE YOUR LOCATION'
-                        : getDistance(mylocation!, latLng).toString() +
-                            "KM AWAY",
-                    snippet: latLng.toString()),
+                infoWindow:
+                    InfoWindow(title: 'CUDDALORE', snippet: latLng.toString()),
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueMagenta),
                 position: latLng,
                 markerId: MarkerId('CUDDALORE')),
             Marker(
-                infoWindow: InfoWindow(
-                    title: isChecked == false
-                        ? 'ENSURE YOUR LOCATION'
-                        : getDistance(mylocation!, latLng2).toString() +
-                            "KM AWAY",
-                    snippet: latLng2.toString()),
+                infoWindow:
+                    InfoWindow(title: 'CHENNAI', snippet: latLng2.toString()),
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueAzure),
                 position: latLng2,
                 markerId: MarkerId('CHENNAI')),
             Marker(
                 infoWindow: InfoWindow(
-                    title: isChecked == false
-                        ? 'ENSURE YOUR LOCATION'
-                        : getDistance(mylocation!, latLng3).toString() +
-                            "KM AWAY",
-                    snippet: latLng3.toString()),
+                    title: 'PUDHUCHERRY', snippet: latLng3.toString()),
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueViolet),
                 position: latLng3,
                 markerId: MarkerId('PUDHUCHERRY')),
             Marker(
-                infoWindow: InfoWindow(
-                    title: isChecked == false
-                        ? 'ENSURE YOUR LOCATION'
-                        : getDistance(mylocation!, latLng4).toString() +
-                            "KM AWAY",
-                    snippet: latLng4.toString()),
+                infoWindow:
+                    InfoWindow(title: 'HARYANA', snippet: latLng4.toString()),
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueOrange),
                 position: latLng4,
                 markerId: MarkerId('HARYANA')),
             Marker(
-                infoWindow: InfoWindow(
-                    title: isChecked == false
-                        ? 'ENSURE YOUR LOCATION'
-                        : getDistance(mylocation!, latLng5).toString() +
-                            "KM AWAY",
-                    snippet: latLng5.toString()),
+                infoWindow:
+                    InfoWindow(title: 'TRIPURA', snippet: latLng5.toString()),
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueCyan),
                 position: latLng5,
                 markerId: MarkerId('TRIPURA')),
-            if (isChecked == true) tnMarker1
           },
           mapType: MapType.normal,
           onMapCreated: onMapCreated,
           initialCameraPosition:
-              CameraPosition(tilt: 74, zoom: 4, target: latLng3)),
+              CameraPosition(tilt: 74, zoom: 9, target: latLng)),
     );
   }
-}
-
-getDistance(LatLng from, to) {
-  double distance = Geolocator.distanceBetween(
-      from.latitude, from.longitude, to.latitude, to.longitude);
-  String string = "";
-
-  print(distance.toString());
-  string = distance.truncate().toString();
-  return string.substring(0, string.length - 3);
 }
